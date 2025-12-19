9a5c5728531b69fd2986b12ba7905ce62d628000
cye2020 <ariaplay101@gmail.com>
2025-12-19 11:33:49 +0900

    [feat] 텍스트 처리 시간 순 추출

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,7 @@
 import sys
 import uuid
 from pathlib import Path
+import os
 
 def is_running_in_notebook():
     """
@@ -25,28 +26,49 @@ def uuid5_from_str(x: str | None) -> str | None:
         return None
     return str(uuid.uuid5(UUID_NAMESPACE, x))
 
-def increment_path(path, exist_ok=False, sep='', mkdir=False):
-    """
-    YOLO 방식 경로 증가 함수
-    예: runs/detect/exp -> runs/detect/exp2, exp3 ...
-    """
-    path = Path(path)
+def increment_path(path: str | Path, exist_ok: bool = False, sep: str = "", mkdir: bool = False) -> Path:
+    """Increment a file or directory path, i.e., runs/exp --> runs/exp{sep}2, runs/exp{sep}3, ... etc.
 
+    If the path exists and `exist_ok` is not True, the path will be incremented by appending a number and `sep` to the
+    end of the path. If the path is a file, the file extension will be preserved. If the path is a directory, the number
+    will be appended directly to the end of the path.
+
+    Args:
+        path (str | Path): Path to increment.
+        exist_ok (bool, optional): If True, the path will not be incremented and returned as-is.
+        sep (str, optional): Separator to use between the path and the incrementation number.
+        mkdir (bool, optional): Create a directory if it does not exist.
+
+    Returns:
+        (Path): Incremented path.
+
+    Examples:
+        Increment a directory path:
+        >>> from pathlib import Path
+        >>> path = Path("runs/exp")
+        >>> new_path = increment_path(path)
+        >>> print(new_path)
+        runs/exp2
+
+        Increment a file path:
+        >>> path = Path("runs/exp/results.txt")
+        >>> new_path = increment_path(path)
+        >>> print(new_path)
+        runs/exp/results2.txt
+    """
+    path = Path(path)  # os-agnostic
     if path.exists() and not exist_ok:
-        base = path.stem
-        suffix = path.suffix
-        parent = path.parent
+        path, suffix = (path.with_suffix(""), path.suffix) if path.is_file() else (path, "")
 
-        i = 2
-        while True:
-            new_path = parent / f"{base}{sep}{i}{suffix}"
-            if not new_path.exists():
-                path = new_path
+        # Method 1
+        for n in range(2, 9999):
+            p = f"{path}{sep}{n}{suffix}"  # increment path
+            if not os.path.exists(p):
                 break
-            i += 1
+        path = Path(p)
 
     if mkdir:
-        path.mkdir(parents=True, exist_ok=True)
+        path.mkdir(parents=True, exist_ok=True)  # make directory
 
     return path
 
